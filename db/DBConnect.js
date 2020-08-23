f7feae2fe60ac85e640b4c8f3bde3fabee78c74b
ronewa <ronewamavhungu@gmail.com>
2020-08-23 10:20:07 +0200

    adding atlas database

--- db/DBConnect.js
+++ db/DBConnect.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const config = require('../config');
 //const uri = process.env.ATLAS_URI;
 
-mongoose.connect('mongodb://localhost:27017/test-schedule',{
+//mongoose.connect('mongodb://localhost:27017/test-schedule',{
 
-//mongoose.connect( config.atlat_uri,{
+mongoose.connect( config.atlat_uri,{
     useUnifiedTopology:true,
     useNewUrlParser:true,
     useCreateIndex: true
