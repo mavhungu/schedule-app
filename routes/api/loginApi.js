928bb7ca0ae4861b3fb822bb20db0031ac6a3577
ronewa <ronewamavhungu@gmail.com>
2021-02-22 06:17:34 +0200

    re-arranging login & register api

--- routes/api/loginApi.js
+++ routes/api/loginApi.js
@@ -1,37 +1,25 @@
 var express = require('express');
-//var usersRouter = require('../users');
+var bcrypt = require("bcrypt");
 var Users = require('../../db/models/UsersModel');
-//var registeredUsersRoute = require('./registerApi');
 var router = express.Router();
 
 router.post('/login', async (req, res, next)=>{
-    try{
-      let userData = req.body;
-        //let username = userData.name.trim().toLowerCase();
-        let email = userData.email.trim().toLowerCase();
-        let password = userData.password.trim().toLowerCase();
 
-        if(!password || !email){
-          return {
-            error: "Email / Password is empty"
-          }
-        }
-        Users.findOne({email:email, password:password})
-          .then((data)=>{
-            if(!data){
-              return res.send("No user has been found")
-            }
-            console.log("We are in");
-            console.log(data);
-            console.log(data.email);
-            //res.redirect('/users');
-          }).catch((e)=>{
-            error: "Something went wrong"
-          });
-          //let registeredUsers = await Users.find().sort({ name: 'asc'})
-            //console.log(registeredUsers);
+  let userData = req.body;
+  let email = userData.email.trim().toLowerCase();
+  let password = userData.password.trim().toLowerCase();
+  
+  if(!password || !email){
+    return {
+      error: "Email / Password is empty"
+    }
+  }
+    try{
+      let user = await Users.findOne({email:email})
+      let userpassword = await bcrypt.compare(password,user.password)
+      console.log(userpassword)
     }catch(error){
-      res.status(500).send("error" + error);
+      res.status(500).send(error);
     }
   });
 
--- routes/api/registerApi.js
+++ routes/api/registerApi.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bcrypt = require("bcrypt");
 //var usersRouter = require('../users');
 var Users = require('../../db/models/UsersModel');
 //var registeredUsersRoute = require('./registerApi');
@@ -6,27 +7,25 @@ var router = express.Router();
 
 
 router.post('/registerUser', async (req, res, next)=>{
-    try{
-      let userData = req.body;
-        let name = userData.name.trim().toLowerCase();
-        let email = userData.email.trim().toLowerCase();
-        let password1 = userData.password1.trim().toLowerCase();
-        let password2 = userData.password2.trim().toLowerCase();
-        let password = password1;
+  
+  let userData = req.body;
+  let name = userData.name.trim().toLowerCase();
+  let email = userData.email.trim().toLowerCase();
+  let password1 = userData.password1.trim().toLowerCase();
+  let password2 = userData.password2.trim().toLowerCase();
+  let password = await bcrypt.hash(password1,8);
 
-        if(!name || !password1 || !email){
-          return "Username / Email / Password is empty";
-        }
+  if(!name || !password1 || !email){
+    return "Username / Email / Password is empty";
+  }
 
-          let users = new Users({name, email, password})
-          users.save().then(()=>{
-            console.log("information have been saved")
-            res.redirect('/');
-          }).catch((e)=>{
-            return {error: "Something went wrong"+ e };
-          });
-          /*let registeredUsers = await Users.find().sort({ name: 'asc'})
-            console.log(registeredUsers);*/
+    let users = new Users({name, email, password})  
+  try{
+
+    await users.save()
+    console.log("information have been saved")
+    res.redirect('/');
+
     }catch(e){
       res.status(500).send("Error"+ e)
     }
