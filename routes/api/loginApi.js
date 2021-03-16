47347496b9d8492f32db77410b4b9ff72e3badb7
ronewa <ronewamavhungu@gmail.com>
2021-03-17 00:33:08 +0200

    removing password and tokens

--- routes/api/loginApi.js
+++ routes/api/loginApi.js
@@ -5,11 +5,12 @@ var router = express.Router();
 router.post('/login', async (req, res, next)=>{
 
   if(!req.body.password || !req.body.email){
-    throw new Error ("Email / Password is empty");
+    throw new Error ();
   }
     try{
       let user = await Users.findByCredentials(req.body.email,req.body.password);
       let token = await user.generateAuthToken();
+      console.log({user: user.getPublicProfile(), token});
       res.cookie('t', token, {
         expire: new Date() + 9999
       });
@@ -23,8 +24,8 @@ router.post('/login', async (req, res, next)=>{
         }
       });*/
       return res.redirect('/users');
-    }catch(error){
-      res.status(401).send(error);
+    }catch(e){
+      res.status(401).send({error:"Email / Password is empty"});
     }
   });
 
